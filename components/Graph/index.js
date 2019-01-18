d1fea24aa759affdac5fc3d63382708e67f8436e
srmward <srmward@gmail.com>
2019-01-17 21:02:23 -0800

    Custom Style Graph Example

--- components/Graph/index.js
+++ components/Graph/index.js
@@ -3,4 +3,6 @@ import { jsx, css } from '@emotion/core'
 import React from 'react'
 import { testStyles } from './styles.js'
 
-export default ({ text, css }) => <p css={{ ...testStyles, ...css }}>{text}</p>
+export default ({ text, styles }) => (
+  <p css={{ ...testStyles, ...styles }}>{text}</p>
+)
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,13 +1,24 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
 import React from 'react'
 import Graph from '../components/Graph'
 import Button from '../components/Button'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
+const customStyle = css`
+  background-color: black;
+  color: red;
+  font-family: courier;
+  padding: 20px 5px;
+`
+
 storiesOf('Button', module).add('default', () => (
   <Button onClick={action('I was clicked')}>Hello, Brutal button</Button>
 ))
 
-storiesOf('Graph', module).add('default', () => (
-  <Graph text="I am a sample graph." />
-))
+storiesOf('Graph', module)
+  .add('default', () => <Graph text="I am a sample graph." />)
+  .add('custom styles', () => (
+    <Graph text="Wow, I'm stylish." styles={customStyle} />
+  ))
