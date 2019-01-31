1048f08dc46de443c0a535547f9d28509d27d836
srmward <srmward@gmail.com>
2019-01-31 08:02:09 -0800

    Better Type Declaration

--- src/theming/type.js
+++ src/theming/type.js
@@ -1,11 +1,8 @@
-export const TYPE = {
+const TYPE = {
   PRIMARY: 'Courier, sans-serif',
-  SECONDARY:
-    '-apple-system, BlinkMacSystemFont, HelveticaNeue, Helvetica, Roboto, Arial, sans-serif',
-  CAPTION:
-    '-apple-system, BlinkMacSystemFont, HelveticaNeue, Helvetica, Roboto, Arial, sans-serif',
-  LABEL:
-    '-apple-system, BlinkMacSystemFont, HelveticaNeue, Helvetica, Roboto, Arial, sans-serif',
-  LINK:
-    '-apple-system, BlinkMacSystemFont, HelveticaNeue, Helvetica, Roboto, Arial, sans-serif',
 }
+
+TYPE.SECONDARY = TYPE.CAPTION = TYPE.LABEL = TYPE.LINK =
+  '-apple-system, BlinkMacSystemFont, HelveticaNeue, Helvetica, Roboto, Arial, sans-serif'
+
+export { TYPE }
