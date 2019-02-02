1dc027d3fab313b13e84db23f085d0b5c11878f9
srmward <srmward@gmail.com>
2019-02-02 15:47:23 -0800

    Style the "All" Story

--- src/stories/All.stories.js
+++ src/stories/All.stories.js
@@ -3,6 +3,8 @@ import { jsx, css } from '@emotion/core'
 import React from 'react'
 import Button from '../components/Button'
 import Container from '../components/Container'
+import { LAYOUTS } from '../theming/layouts'
+import { COLORS } from '../theming/colors'
 import Graph from '../components/Graph'
 import Headline from '../components/Headline'
 import Input from '../components/Input'
@@ -19,12 +21,19 @@ const customStyleHeadline = css`
   padding: 20px 5px;
 `
 
+const containerhStyles = css`
+${LAYOUTS.FLEX_JUSTIFY_CENTER};
+${LAYOUTS.FULL_HEIGHT}
+border: 1px solid ${COLORS.BLUE};
+  padding: 100px 20px;
+`
+
 storiesOf('All', module)
   .add('Default - Home', () => (
     <>
       <Headline text="REACT BRUTALIST UI" styles={customStyleHeadline} />
       <Button onClick={action('I was clicked')}>Hello, Brutal button</Button>
-      <Container>
+      <Container styles={containerhStyles}>
         <p>I cannot be contained.</p>
       </Container>
       <Graph text="I am a sample graph" />
